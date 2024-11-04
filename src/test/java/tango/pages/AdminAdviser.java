86bd8411133c056d13f63a1db9c3fb64b61e26d1
unknown <tanvywa@gmail.com>
2024-11-04 17:23:20 +0700

    4/11

--- src/test/java/tango/pages/AdminAdviser.java
+++ src/test/java/tango/pages/AdminAdviser.java
@@ -1,4 +1,26 @@
 package tango.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import static tango.Untils.WaitLoadPage;
+
 public class AdminAdviser {
+    WebDriver driver;
+    public AdminAdviser(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver, this);
+    }
+    public boolean checkAdminAdviser (WebDriver driver){
+        try {
+            WebElement textAdviser = WaitLoadPage(driver, 15, By.xpath("//p[.='Profile - Name']"));
+            return textAdviser.getText().contains("Profile-Name");
+        }
+        catch (TimeoutException e){
+            return false;
+        }
+    }
 }
--- src/test/java/tango/pages/AdminContractor.java
+++ src/test/java/tango/pages/AdminContractor.java
@@ -1,4 +1,26 @@
 package tango.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import static tango.Untils.WaitLoadPage;
+
 public class AdminContractor {
+    WebDriver driver;
+    public AdminContractor(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver, this);
+    }
+    public boolean checkAdminContractor (WebDriver driver){
+        try {
+            WebElement textContractor = WaitLoadPage(driver, 15, By.xpath("//p[.='Profile - Name']"));
+            return textContractor.getText().contains("Profile-Name");
+        }
+        catch (TimeoutException e){
+            return false;
+        }
+    }
 }
